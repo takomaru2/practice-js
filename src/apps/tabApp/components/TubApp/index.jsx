1904033a258ac122611075d5bc5390c36d4f2ee3
takomaru2 <col.dd93@gmail.com>
2025-10-29 11:51:01 +0900

    feat: 押されたボタンに対応するメッセージを表示

--- src/apps/tabApp/components/TubApp/index.jsx
+++ src/apps/tabApp/components/TubApp/index.jsx
@@ -1,13 +1,36 @@
-import { BottomTabButton } from "../BottomTab/index.jsx";
+import { BottomTabButton } from "../BottomTabButton/index.jsx";
 import styles from "./index.module.scss";
+import { useState } from "react";
 export const TabApp = () => {
+  const [state, setState] = useState("ああああ");
+
+  const message = { hoge: "ああああ", fuga: "っっっい", piyo: "にににい" };
+
   return (
     <div>
-      <div className={styles.tabContent}>コンテナ層</div>
+      <div className={styles.tabContent}>{state}</div>
       <div className={styles.buttonNav}>
-        <BottomTabButton icon="🏠" name="ホーム" />
-        <BottomTabButton icon="🔍" name="検索" />
-        <BottomTabButton icon="🔔" name="通知" />
+        <BottomTabButton
+          icon="🏠"
+          name="ホーム"
+          onClick={() => {
+            setState(message.hoge);
+          }}
+        />
+        <BottomTabButton
+          icon="🔍"
+          name="検索"
+          onClick={() => {
+            setState(message.fuga);
+          }}
+        />
+        <BottomTabButton
+          icon="🔔"
+          name="通知"
+          onClick={() => {
+            setState(message.piyo);
+          }}
+        />
       </div>
     </div>
   );
