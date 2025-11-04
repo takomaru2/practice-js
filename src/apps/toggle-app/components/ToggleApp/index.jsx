cbe836ec8873fdbdccac80ee66ce69a50ad67d55
takomaru2 <col.dd93@gmail.com>
2025-11-04 22:44:19 +0900

    feat: ToggleAppのベース構造を追加

--- src/apps/toggle-app/components/ToggleApp/index.jsx
+++ src/apps/toggle-app/components/ToggleApp/index.jsx
@@ -1,7 +1,16 @@
+import styles from "./index.module.scss";
+
 export const ToggleApp = () => {
+  const hoge = ["on", "off"];
+
   return (
-    <div>
-      <div>ToggleApp</div>
+    <div className={styles.container}>
+      <div className={styles.switchWrapper}>
+        <button className={styles.switch}>
+          <span className={styles.slider}></span>
+        </button>
+        <div>{hoge[0]}</div>
+      </div>
     </div>
   );
 };
