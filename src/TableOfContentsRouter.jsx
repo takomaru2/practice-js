3ce4fbcbf132974e89b1b7285b476d355693fe92
takomaru2 <col.dd93@gmail.com>
2025-11-06 17:32:38 +0900

    refactor: 名前重要

--- src/TableOfContentsRouter.jsx
+++ src/TableOfContentsRouter.jsx
@@ -10,13 +10,13 @@ const pages = [
 export const TableOfContentsPage = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Practice App</h1>
+      <h1 className={styles.pageTitle}>Practice App</h1>
       <div>
-        <h2 className={styles.subTitle}>初級 ⭐︎</h2>
-        <ul className={styles.listWrapper}>
+        <h2 className={styles.difficulty}>初級 ⭐︎</h2>
+        <ul className={styles.linkToAppWrapper}>
           {pages.map((page) => (
             <li key={page.path}>
-              <Link className={styles.list} to={page.path}>
+              <Link className={styles.linkToApp} to={page.path}>
                 {page.name}
               </Link>
             </li>
