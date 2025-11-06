0795d561af7b7c760618589c91d9fa23a0b0f34e
takomaru2 <col.dd93@gmail.com>
2025-11-06 15:22:51 +0900

    feat: 要件通りの機能実装完了！

--- src/apps/likeApp/components/LikeApp/index.jsx
+++ src/apps/likeApp/components/LikeApp/index.jsx
@@ -2,12 +2,25 @@ import styles from "./index.module.scss";
 import { useState } from "react";
 
 export const LikeApp = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
     <div className={styles.container}>
-      <button className={styles.like}>♥ いいねっ</button>
+      <button className={styles.like} onClick={handleClick}>
+        ♥ いいねっ
+      </button>
       <span className={styles.counter}>{count}</span>
-      <button className={styles.reset}>リセッツ</button>
+      <button className={styles.reset} onClick={handleReset}>
+        リセッツ
+      </button>
     </div>
   );
 };
