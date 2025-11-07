d8532072e3b74979631a2a529a5688373e1cf2a3
takomaru2 <col.dd93@gmail.com>
2025-11-07 15:44:34 +0900

    feat: 要件通りの機能実装終了！

--- src/apps/darkModeApp/component/DarkModeApp/index.jsx
+++ src/apps/darkModeApp/component/DarkModeApp/index.jsx
@@ -4,9 +4,16 @@ import { useState } from "react";
 export const DarkModeApp = () => {
   const [isDark, setIsDark] = useState(false);
 
+  const handle = () => {
+    setIsDark((prev) => {
+      return !prev;
+    });
+  };
+
   return (
     <div className={`${styles.content} ${isDark ? styles.contentDark : ""}`}>
       <button
+        onClick={handle}
         className={`${styles.button} ${isDark ? styles.dark : styles.light} `}
       >
         {isDark ? "ライトモードに切り替え" : "ダークモードに切り替え"}
