daf3b09bae79c447207a178a02560f19d7887b22
takomaru2 <col.dd93@gmail.com>
2025-11-07 15:38:30 +0900

    feat: 静的実装

--- src/apps/darkModeApp/component/DarkModeApp/index.jsx
+++ src/apps/darkModeApp/component/DarkModeApp/index.jsx
@@ -1,7 +1,16 @@
+import styles from "./index.module.scss";
+import { useState } from "react";
+
 export const DarkModeApp = () => {
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <div>
-      <div>ダークモード切り替えアプリ</div>
+    <div className={`${styles.content} ${isDark ? styles.contentDark : ""}`}>
+      <button
+        className={`${styles.button} ${isDark ? styles.dark : styles.light} `}
+      >
+        {isDark ? "ライトモードに切り替え" : "ダークモードに切り替え"}
+      </button>
     </div>
   );
 };
