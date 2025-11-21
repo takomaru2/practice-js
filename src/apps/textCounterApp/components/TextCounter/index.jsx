2ce4891d82db14b70b5dde284d2579c4e717e713
takomaru2 <col.dd93@gmail.com>
2025-11-21 17:19:59 +0900

    feat: 静的実装

--- src/apps/textCounterApp/components/TextCounter/index.jsx
+++ src/apps/textCounterApp/components/TextCounter/index.jsx
@@ -1,7 +1,26 @@
+import styles from "./index.module.scss";
+
 export const TextCounter = () => {
   return (
-    <div>
-      <div>テキストカウンター</div>
+    <div className={styles.container}>
+      <h1>テキストカウンター</h1>
+      <div className={styles.counterWrapper}>
+        <div className={styles.counterItem}>
+          <div>文字数 </div>
+          <span>0</span>
+        </div>
+        <div className={styles.counterItem}>
+          <div>行数 </div>
+          <span>0</span>
+        </div>
+      </div>
+      <textarea
+        className={styles.textArea}
+        placeholder={"ここに書けやれい"}
+      ></textarea>
+      <div className={styles.hoge}>
+        <button className={styles.clearButton}>全消しンゴ</button>
+      </div>
     </div>
   );
 };
