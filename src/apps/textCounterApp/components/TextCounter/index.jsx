04d1ea4ec9b9019fb8166926c2c3a3e5f77822a0
takomaru2 <col.dd93@gmail.com>
2025-11-27 17:09:46 +0900

    feat: 文字数カウンターと行数カウンターを実装

--- src/apps/textCounterApp/components/TextCounter/index.jsx
+++ src/apps/textCounterApp/components/TextCounter/index.jsx
@@ -5,30 +5,41 @@ export const TextCounter = () => {
   //テキストエリアの文字数が1文字以上だったらクリアボタンを出現させる
 
   // テキストエリアの文字数を見ないといけない
+
   const [text, setText] = useState("");
   const isActive = text.length > 0;
+
+  const lineCount = text.split("\n").length;
+  //文字数
+  // todo: 改行した時に文字数をカウントするな
+  // todo: スペースを入力した時に文字数カウントするな
+  //行数
+  // todo: ０文字時は行数は０だが１文字以降は１行とカウントする
+
   return (
     <div className={styles.container}>
       <h1>テキストカウンター</h1>
       <div className={styles.counterWrapper}>
         <div className={styles.counterItem}>
           <div>文字数</div>
-          <span>0</span>
+          <span>{text.length}</span>
         </div>
         <div className={styles.counterItem}>
           <div>行数</div>
-          <span>0</span>
+          <span>{lineCount}</span>
         </div>
       </div>
       <textarea
         className={styles.textArea}
         value={text}
-        onChange={(hoge) => setText(hoge.target.value)}
+        onChange={(event) => setText(event.target.value)}
         placeholder={"ここに書けやれい"}
-      ></textarea>
-      <div className={styles.hoge}>
-        {isActive && <button className={styles.clearButton}>全消しンゴ</button>}
-      </div>
+      />
+      {isActive && (
+        <div className={styles.hoge}>
+          <button className={styles.clearButton}>全消しンゴ</button>
+        </div>
+      )}
     </div>
   );
 };
