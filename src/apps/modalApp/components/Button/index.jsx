8c900a24cbbd38bd6342eb7bdae552d2dbabe9a0
takomaru2 <col.dd93@gmail.com>
2025-12-02 12:54:40 +0900

    feat: 静的実装

---
+++ src/apps/modalApp/components/Button/index.jsx
@@ -0,0 +1,5 @@
+import styles from "./index.module.scss";
+
+export const Button = ({ label }) => {
+  return <button className={styles.fuga}>{label}</button>;
+};
--- src/apps/modalApp/components/ModalApp/index.jsx
+++ src/apps/modalApp/components/ModalApp/index.jsx
@@ -1,9 +1,15 @@
 import styles from "./index.module.scss";
+import { Button } from "../Button/index.jsx";
 
 export const ModalApp = () => {
   return (
-    <div>
+    <div className={styles.aaa}>
       <h1 className={styles.hoge}>名言モーダル</h1>
+      <div className={styles.buttonGroup}>
+        <Button label={"網走の人"} />
+        <Button label={"米子育ちの人"} />
+        <Button label={"考える人"} />
+      </div>
     </div>
   );
 };
