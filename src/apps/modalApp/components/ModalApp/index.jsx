b07a335228c784cb4ca3e3ded0b4c66988a35aed
takomaru2 <col.dd93@gmail.com>
2025-12-03 15:45:27 +0900

    refactor: ボタン生成をmap化

--- src/apps/modalApp/components/ModalApp/index.jsx
+++ src/apps/modalApp/components/ModalApp/index.jsx
@@ -8,7 +8,7 @@ export const ModalApp = () => {
   const showModal = (message) => setModal({ isOpen: true, message });
   const closeModal = () => setModal({ isOpen: false, message: "" });
 
-  const boke = [
+  const bokes = [
     { key: 1, label: "網走の人", message: "捕まってしまいました" },
     { key: 2, label: "米子育ちの人", message: "ここは日本の首都さ！" },
     { key: 3, label: "考える人", message: "んーー難しい問題だね" },
@@ -18,20 +18,17 @@ export const ModalApp = () => {
     <div className={styles.aaa}>
       <h1 className={styles.hoge}>名言モーダル</h1>
       <div className={styles.buttonGroup}>
-        <Button
-          label={boke[0].label}
-          showModal={() => showModal(boke[0].message)}
-        />
-        <Button
-          label={boke[1].label}
-          showModal={() => showModal(boke[1].message)}
-        />
-        <Button
-          label={boke[2].label}
-          showModal={() => showModal(boke[2].message)}
-        />
+        {bokes.map((boke) => (
+          <Button
+            label={boke.label}
+            showModal={() => {
+              showModal(boke.message);
+            }}
+          />
+        ))}
       </div>
 
+      {/*ここはボタンコンポーネントの責務なのか、アコーディオンはそう*/}
       {modal.isOpen && (
         <>
           <div className={styles.backDrop}></div>
