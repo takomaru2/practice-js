157f43d70244b332e4c52831520903a9b59aa5ef
takomaru2 <col.dd93@gmail.com>
2025-12-03 18:36:23 +0900

    chore: 不必要だったkeyを消した

--- src/apps/modalApp/components/ModalApp/index.jsx
+++ src/apps/modalApp/components/ModalApp/index.jsx
@@ -4,9 +4,9 @@ import { useState } from "react";
 import { Modal } from "../Modal/index.jsx";
 
 const BOKES = [
-  { key: 1, label: "網走の人", message: "捕まってしまいました" },
-  { key: 2, label: "米子育ちの人", message: "ここは日本の首都さ！" },
-  { key: 3, label: "考える人", message: "んーー難しい問題だね" },
+  { label: "網走の人", message: "捕まってしまいました" },
+  { label: "米子育ちの人", message: "ここは日本の首都さ！" },
+  { label: "考える人", message: "んーー難しい問題だね" },
 ];
 
 export const ModalApp = () => {
