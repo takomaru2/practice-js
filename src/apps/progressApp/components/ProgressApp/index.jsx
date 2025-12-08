ce3a5fea61c26aee4df318effe8d6d1f488f154f
takomaru2 <col.dd93@gmail.com>
2025-12-08 18:31:30 +0900

    feat: 静的実装

--- src/apps/progressApp/components/ProgressApp/index.jsx
+++ src/apps/progressApp/components/ProgressApp/index.jsx
@@ -1,7 +1,27 @@
+import styles from "./index.module.scss";
+
 export const ProgressApp = () => {
   return (
-    <div>
-      <h1>Progress Bar</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Progress Bar</h1>
+      <div className={styles.card}>
+        <div className={styles.progress}>
+          <div className={styles.bar}></div>
+          <span className={styles.pa}>0%</span>
+        </div>
+        <div className={styles.controllers}>
+          <div className={styles.hoge}>
+            <button className={styles.minusButton}>-10</button>
+            <button className={styles.minusButton}>-5</button>
+            <button className={styles.addButton}>+5</button>
+            <button className={styles.addButton}>+10</button>
+          </div>
+          <div className={styles.fuga}>
+            <button className={styles.resetButton}>リセット</button>
+            <button className={styles.controlButton}>完了</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
