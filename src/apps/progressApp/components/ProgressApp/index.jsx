61b815c767d731d416c591b5c29413cb70ea9340
takomaru2 <col.dd93@gmail.com>
2025-12-09 15:20:14 +0900

    feat: プログレスバーの進捗反映を実装

--- src/apps/progressApp/components/ProgressApp/index.jsx
+++ src/apps/progressApp/components/ProgressApp/index.jsx
@@ -1,21 +1,46 @@
 import styles from "./index.module.scss";
+import { useState } from "react";
 
 export const ProgressApp = () => {
+  const [progress, setProgress] = useState(0);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Progress Bar</h1>
       <div className={styles.card}>
         <div className={styles.progress}>
           <div className={styles.bar}></div>
-          <div className={styles.activeBar}></div>
-          <span className={styles.pa}>0%</span>
+          <div
+            className={styles.activeBar}
+            style={{ width: `${progress}%` }}
+          ></div>
+          <span className={styles.pa}>{progress}%</span>
         </div>
         <div className={styles.controllers}>
           <div className={styles.hoge}>
-            <button className={styles.minusButton}>-10</button>
-            <button className={styles.minusButton}>-5</button>
-            <button className={styles.addButton}>+5</button>
-            <button className={styles.addButton}>+10</button>
+            <button
+              className={styles.minusButton}
+              onClick={() => setProgress(progress - 10)}
+            >
+              -10
+            </button>
+            <button
+              className={styles.minusButton}
+              onClick={() => setProgress(progress - 5)}
+            >
+              -5
+            </button>
+            <button
+              className={styles.addButton}
+              onClick={() => setProgress(progress + 5)}
+            >
+              +5
+            </button>
+            <button
+              className={styles.addButton}
+              onClick={() => setProgress(progress + 10)}
+            >
+              +10
+            </button>
           </div>
           <div className={styles.fuga}>
             <button className={styles.resetButton}>リセット</button>
