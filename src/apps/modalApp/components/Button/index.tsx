ea8459f08588fd0688b57050ae78d720a8d693d5
takomaru2 <col.dd93@gmail.com>
2025-12-18 18:46:36 +0900

    fix: Butoonのtype error直した

--- src/apps/modalApp/components/Button/index.tsx
+++ src/apps/modalApp/components/Button/index.tsx
@@ -1,6 +1,16 @@
 import styles from "./index.module.scss";
 
-export const Button = ({ speaker, onClick }) => {
+// propsはオブジェクトの形だからこれでええねん
+// これは直接引数に型をつけているのでは無く、先に型を定義してからつけようという作戦
+//speakerは辿っていけばstringなことはわかる
+type ButtonProps = {
+  speaker: string;
+  onClick: () => void;
+};
+//さて問題はonClickだ。onClickには関数が渡されているが、関数型ーで済む話なのか
+//
+
+export const Button = ({ speaker, onClick }: ButtonProps) => {
   return (
     <button className={styles.button} onClick={onClick}>
       {speaker}
