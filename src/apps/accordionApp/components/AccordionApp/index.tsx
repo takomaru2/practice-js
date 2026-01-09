0abba32952d948546d850d77ce86d59d76a79aa2
takomaru2 <col.dd93@gmail.com>
2026-01-09 17:58:56 +0900

    refactor: AccordionAppに適切な型を付けた

--- src/apps/accordionApp/components/AccordionApp/index.tsx
+++ src/apps/accordionApp/components/AccordionApp/index.tsx
@@ -1,30 +1,38 @@
 import styles from "./index.module.scss";
 import { AccordionItem } from "../AccordionItem";
 import { FC, useState } from "react";
-// todo: こっちの型も直してあげろよ
-const ITEMS = [
+
+type Items = {
+  id: number;
+  question: string;
+  answer: string;
+};
+
+const ITEMS: Items[] = [
   { id: 1, question: "フシギダネ", answer: "草🌱" },
   { id: 2, question: "ゼニガメ", answer: "水💧" },
   { id: 3, question: "ヒトカゲ", answer: "炎🔥" },
 ];
 
 export const AccordionApp: FC = () => {
-  const [openIndex, setOpenIndex] = useState<any[]>([]);
+  const [openIndex, setOpenIndex] = useState<number[]>([]);
 
-  const openAll = () => {
+  const openAll: () => void = () => {
     setOpenIndex(ITEMS.map((_, index) => index));
   };
 
-  const closeAll = () => {
+  const closeAll: () => void = () => {
     setOpenIndex([]);
   };
 
-  const toggleItem = (index: any) => {
-    setOpenIndex((prev: any) => {
+  // 引数あるから引数と戻り値それぞれ型を付けたい。
+  // 引数はIndexだからnumberで他の関数と同様にtoggleItemから見ると何も返してないからvoidなのではなかろうかと思う。
+  const toggleItem = (index: number): void => {
+    setOpenIndex((prev) => {
       if (prev.includes(index)) {
         // 開いている=>閉じる
-        //閉じたいので既に空いている状態。結果prevからindexと同じものだけ消える。
-        return prev.filter((i: any) => i !== index);
+        // 閉じたいので既に空いている状態。結果prevからindexと同じものだけ消える。
+        return prev.filter((i) => i !== index);
       } else {
         // 閉じている=>開く
         return [...prev, index];
