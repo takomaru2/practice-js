8ac673878d51b7f207b3be40de6e423ea005af84
takomaru2 <col.dd93@gmail.com>
2026-01-16 16:51:54 +0900

    feat: 数値1・数値2・演算子のstateと入力を実装

--- src/apps/calculatorApp/components/CalculatorApp/index.tsx
+++ src/apps/calculatorApp/components/CalculatorApp/index.tsx
@@ -1,6 +1,14 @@
 import styles from "./index.module.scss";
+import { useState } from "react";
 
 export const CalculatorApp = () => {
+  const [firstValue, setFirstValue] = useState<string | undefined>(undefined);
+  const [secondValue, setSecondValue] = useState<string | undefined>(undefined);
+  const [operator, setOperator] = useState<string>("+");
+  // const [result, setResult] = useState<number | null>(null);
+  // const [error, setError] = useState<string | null>(null);
+  // const [show, setShow] = useState(false);
+
   return (
     <div className={styles.container}>
       <div className={styles.card}>
@@ -11,12 +19,19 @@ export const CalculatorApp = () => {
             type="number"
             className={styles.input}
             placeholder={"数値を入力してくれ"}
+            value={firstValue}
+            onChange={(event) => setFirstValue(event.target.value)}
           />
         </label>
 
         <label className={styles.inputGroup}>
           <span className={styles.label}>演算子</span>
-          <select name="selectedFruit" className={styles.input}>
+          <select
+            name="selectedOperator"
+            className={styles.input}
+            value={operator}
+            onChange={(event) => setOperator(event.target.value)}
+          >
             <option>+</option>
             <option>-</option>
             <option>x</option>
@@ -30,8 +45,11 @@ export const CalculatorApp = () => {
             type="number"
             className={styles.input}
             placeholder={"数値を入力してくれ"}
+            value={secondValue}
+            onChange={(event) => setSecondValue(event.target.value)}
           />
         </label>
+
         <div className={styles.buttonGroup}>
           <button className={styles.submitButton}>計算する</button>
           <button className={styles.resetButton}>リセッツ</button>
