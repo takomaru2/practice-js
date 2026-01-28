db52a98a178d4a0ef9a34deb5911a78073ae5535
takomaru2 <col.dd93@gmail.com>
2026-01-28 18:24:35 +0900

    feat: 項目checkboxの中間状態をuseRefで無理矢理実装

--- src/apps/checkBoxApp/components/CheckBoxApp/index.tsx
+++ src/apps/checkBoxApp/components/CheckBoxApp/index.tsx
@@ -1,13 +1,12 @@
 import styles from "./index.module.scss";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export const CheckBoxApp = () => {
   const [check1, setCheck1] = useState(false);
   const [check2, setCheck2] = useState(false);
-  const [check3, setCheck3] = useState(false);
 
-  const hasChecked = check1 || check2 || check3;
-  const checks = [check1, check2, check3];
+  const hasChecked = check1 || check2;
+  const checks = [check1, check2];
   // trueなやつ数える。filterがtruthyなものを返すのだ。つまりtrueの数だけ数えれる
   const count = checks.filter(Boolean).length;
   const total = checks.length;
@@ -17,7 +16,37 @@ export const CheckBoxApp = () => {
     const next = !(check1 && check2);
     setCheck1(next);
     setCheck2(next);
-    setCheck3(next);
+  };
+
+  // パーシャルの
+  // 条件　選択されているチェックボックスが１つ以上かつ、全選択はされていない状態
+  // 何をするのか　項目の中身横棒にする
+
+  // const allCheck = check1 && check2 && check3;
+  // const isPartial = hasChecked && !allCheck;
+
+  // 項目チェックボックスを変数に入れて扱えるようにしたい。propsにないのでuseRef使う。
+  const allCheckBoxRef = useRef<HTMLInputElement | null>(null);
+  console.log(
+    "ほげ",
+    allCheckBoxRef.current && allCheckBoxRef.current.indeterminate,
+  );
+
+  // useEffect(() => {
+  //   console.log("allCheckBoxRef", allCheckBoxRef);
+  // }, []);
+
+  // useEffect(() => {
+  //   if (allCheckBoxRef.current) {
+  //     allCheckBoxRef.current.indeterminate = isPartial;
+  //   }
+  // }, [isPartial]);
+
+  const updatePartial = (newIsPartial: boolean) => {
+    if (allCheckBoxRef.current) {
+      console.log("FUGA", newIsPartial);
+      allCheckBoxRef.current.indeterminate = newIsPartial;
+    }
   };
 
   return (
@@ -28,6 +57,7 @@ export const CheckBoxApp = () => {
           <tr className={styles.row}>
             <th className={styles.checkBoxColumn}>
               <input
+                ref={allCheckBoxRef}
                 type="checkbox"
                 checked={hasChecked}
                 onChange={checkAllFn}
@@ -44,7 +74,14 @@ export const CheckBoxApp = () => {
               <input
                 type="checkbox"
                 checked={check1}
-                onChange={() => setCheck1(!check1)}
+                onChange={() => {
+                  setCheck1(!check1);
+                  const newAllCheck = !check1 && check2;
+                  const newHasChecked = !check1 || check2;
+                  const newIsPartial = newHasChecked && !newAllCheck;
+
+                  updatePartial(newIsPartial);
+                }}
               />
             </th>
             <th className={styles.cell}>スクワット</th>
@@ -56,28 +93,21 @@ export const CheckBoxApp = () => {
               <input
                 type="checkbox"
                 checked={check2}
-                onChange={() => setCheck2(!check2)}
+                onChange={() => {
+                  setCheck2(!check2);
+                  const newAllCheck = check1 && !check2;
+                  const newHasChecked = check1 || !check2;
+                  const newIsPartial = newHasChecked && !newAllCheck;
+
+                  updatePartial(newIsPartial);
+                }}
               />
             </th>
             <th className={styles.cell}>ダッシュ</th>
             <th className={styles.cell}>120分</th>
           </tr>
-
-          <tr className={styles.fuga}>
-            <th className={styles.checkBoxColumn}>
-              <input
-                type="checkbox"
-                checked={check3}
-                onChange={() => setCheck3(!check3)}
-              />
-            </th>
-            <th className={styles.cell}>朝ごはん</th>
-            <th className={styles.cell}>1分</th>
-          </tr>
         </tbody>
       </table>
-      {/*チェック１つでもついていたら出てくる*/}
-      {/* todo:countの部分が可変になる */}
       {hasChecked && (
         <div className={styles.count}>
           選択中:{count}/{total}
