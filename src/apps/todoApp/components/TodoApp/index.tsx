5ba8f0e5b113e48607eda1445591b50a96deb60b
takomaru2 <col.dd93@gmail.com>
2026-03-03 18:14:47 +0900

    refactor: 命名を変更し、コンポーネント化して可読性をあげた

--- src/apps/todoApp/components/TodoApp/index.tsx
+++ src/apps/todoApp/components/TodoApp/index.tsx
@@ -1,10 +1,82 @@
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 
 export const TodoApp = () => {
   const [inputtingTaskName, setInputtingTaskName] = useState(""); // 新しいタスクの入力値
   const [taskNames, setTaskNames] = useState<string[]>([]); // 保存しているタスク一覧
-  const [editingText, setEditingText] = useState(""); // 編集中のタスクのテキスト
-  const [editingItem, setEditingItem] = useState<string | null>(null); // 編集中のタスクを特定するための値
+  const [editingText, setEditingText] = useState(""); // 編集中inputの一時的な値
+  const [editingItem, setEditingItem] = useState<string | null>(null); // 編集中のタスクを特定するための値。文字列だと一意ではないのでidが欲しい
+
+  type TaskProps = {
+    taskName: string;
+    onEdit: () => void;
+    onDelete: () => void;
+  };
+
+  const Task = ({ taskName, onEdit, onDelete }: TaskProps) => {
+    return (
+      <div style={{ display: "flex", gap: "10px" }}>
+        <div>{taskName}</div>
+
+        <button style={{ border: "2px lightblue solid" }} onClick={onEdit}>
+          編集
+        </button>
+
+        <button style={{ border: "2px red solid" }} onClick={onDelete}>
+          削除
+        </button>
+      </div>
+    );
+  };
+
+  type EditProps = {
+    editingText: string;
+    onSave: MouseEventHandler<HTMLButtonElement> | undefined;
+    onCancel: MouseEventHandler<HTMLButtonElement> | undefined;
+  };
+
+  const EditTask = ({ editingText, onSave, onCancel }: EditProps) => {
+    return (
+      <div style={{ display: "flex", gap: "10px" }}>
+        <input
+          style={{ border: "2px black solid" }}
+          value={editingText}
+          onChange={(event) => setEditingText(event.target.value)}
+        />
+
+        <button style={{ border: "2px lightgreen solid" }} onClick={onSave}>
+          保存
+        </button>
+
+        <button style={{ border: "2px lightgray solid" }} onClick={onCancel}>
+          キャンセル
+        </button>
+      </div>
+    );
+  };
+
+  const handleCancel = () => {
+    setEditingItem(null);
+  };
+
+  const handleSave = () => {
+    const updated = taskNames.map((task) =>
+      task === editingItem ? editingText : task,
+    );
+    setTaskNames(updated);
+    setEditingItem(null);
+  };
+
+  const handleDelete = (taskName: string) => {
+    const result = taskNames.filter((task) => {
+      return task !== taskName;
+    });
+    setTaskNames(result);
+  };
+
+  const handleEdit = (taskName: string) => {
+    setEditingItem(taskName);
+    setEditingText(taskName);
+  };
 
   return (
     <div style={{ padding: "10px" }}>
@@ -33,65 +105,21 @@ export const TodoApp = () => {
       {taskNames.length > 0 &&
         taskNames.map((taskName) => {
           if (editingItem === taskName) {
-            // 編集UI
             return (
-              <div style={{ display: "flex", gap: "10px" }}>
-                <input
-                  style={{ border: "2px black solid" }}
-                  value={editingText}
-                  onChange={(event) => setEditingText(event.target.value)}
-                />
-
-                <button
-                  style={{ border: "2px lightgreen solid" }}
-                  onClick={() => {
-                    const updated = taskNames.map((task) =>
-                      task === editingItem ? editingText : task,
-                    );
-                    setTaskNames(updated);
-                    setEditingItem(null);
-                  }}
-                >
-                  保存
-                </button>
-
-                <button
-                  style={{ border: "2px lightgray solid" }}
-                  onClick={() => setEditingItem(null)}
-                >
-                  キャンセル
-                </button>
-              </div>
+              <EditTask
+                editingText={editingText}
+                onCancel={handleCancel}
+                onSave={handleSave}
+              />
             );
           }
 
-          // 通常UI
           return (
-            <div style={{ display: "flex", gap: "10px" }}>
-              <div>{taskName}</div>
-
-              <button
-                style={{ border: "2px lightblue solid" }}
-                onClick={() => {
-                  setEditingItem(taskName);
-                  setEditingText(taskName);
-                }}
-              >
-                編集
-              </button>
-
-              <button
-                style={{ border: "2px red solid" }}
-                onClick={() => {
-                  const result = taskNames.filter((task) => {
-                    return task !== taskName;
-                  });
-                  setTaskNames(result);
-                }}
-              >
-                削除
-              </button>
-            </div>
+            <Task
+              taskName={taskName}
+              onDelete={() => handleDelete(taskName)}
+              onEdit={() => handleEdit(taskName)}
+            />
           );
         })}
     </div>
